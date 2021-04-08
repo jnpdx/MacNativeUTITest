0ae2e0283d9ceefd52a3c453dcdf6c879e4d6041
jnpdx <john@johnnastos.com>
2021-04-07 20:29:57 -0700

    Initial commit

---
+++ MacCatalystUTITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacCatalystUTITest
+//
+//  Created by John Nastos on 4/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
